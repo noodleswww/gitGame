a8e343e4e9b3ab95b42fb2b09a0763b6dea381c2
boat <460873206@qq.com>
2014-08-22 14:46:15 +0800

    颜色和吃金币

---
+++ develop/color/Rect.js
@@ -0,0 +1,36 @@
+﻿/*
+*n:当前加载的 方块数目
+*color:默认方块颜色
+*rectColor:点击的方块颜色
+*radius:圆角矩形弧度
+*
+*/
+
+function Rect(n, color, rectColor,radius,gap) {
+	createjs.Shape.call(this);
+
+	this.setRectType = function (type) {
+		this._RectType = type;
+		switch (type) {
+			case 1:
+				this.setColor(color);
+				break;
+			case 2:
+				this.setColor(rectColor);
+				break;
+		}
+	}
+
+	this.setColor = function (colorString) {
+		this.graphics.beginFill(colorString);
+		this.graphics.drawRoundRect(0, 0, (400 - (n - 1) * gap) / n, (400 - (n - 1) * gap) / n, radius);
+		this.graphics.endFill();
+	}
+
+	this.getRectType = function () {
+		return this._RectType;
+	}
+
+	this.setRectType(1);
+}
+Rect.prototype = new createjs.Shape();
